9fc2e4fc1b9f7610f3e9ebbfdb35dbfffb80c7f1
Livedave03 <lopelquelop@gmail.com>
2024-11-13 14:32:11 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // token de Telegram para el bot
-$token = "7832144704:AAHwHS_UaIZqVlgOjTeYdX7OYML5jJfGyAc"; // ejemplo del token, colócalo aquí.
+$token = "7487930696:AAEntMfgs7gStucSx9UIqkKv8q-s5ehNeWw"; // ejemplo del token, colócalo aquí.
 
 // chat_id de Telegram donde quieres enviar los mensajes
 $chat_id = "5157616506"; // el ID de chat que te dará Telegram
